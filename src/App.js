09f45b08eae8deb121b87ddf4767eb8fc3a6f60f
Tsai-Chieh <d0177381@gmail.com>
2022-08-31 13:53:08 +0800

    :sparkles: Creating Forms in React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {useState} from 'react'
 import './App.css'
+import FormInput from './components/FormInput'
 
 const App = () => {
+  const [username, setUsername] = useState('')
+  console.log(username)
+  console.log('re-rendered')
+
   return (
-    <div>Hello world!</div>
+    <div className='app'><form>
+      <FormInput placeholder='Username' setUsername={setUsername}/>
+      <FormInput placeholder='Email'/>
+      <FormInput placeholder='Full Name'/>
+      <FormInput placeholder='Sth Else'/>
+    </form>
+    </div>
   )
 }
 
