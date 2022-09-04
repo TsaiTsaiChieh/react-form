7a09d6dc31e716f60a5a591d24d6f6a7e4d3b81d
Tsai-Chieh <d0177381@gmail.com>
2022-09-04 16:54:21 +0800

    :sparkles: React Form Input Validation and Displaying Errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useRef, useState} from 'react'
+import {useState} from 'react'
 import './App.css'
 import FormInput from './components/FormInput'
 
@@ -15,20 +15,27 @@ const App = () => {
     name: 'username',
     type: 'text',
     placeholder: 'Username',
+    errorMessage: `Username should be 3-16 characters and shouldn't include any 
+    special character`,
     label: 'Username',
+    pattern: '^[A-Za-z]{3,16}$',
+    required: true,
   },
   {
     id: 2,
     name: 'email',
-    type: 'text',
+    type: 'email',
     placeholder: 'Email',
+    errorMessage: 'It should be a valid email address.',
     label: 'Email',
+    required: true,
   },
   {
     id: 3,
     name: 'birthday',
-    type: 'text',
+    type: 'date',
     placeholder: 'Birthday',
+    errorMessage: '',
     label: 'Birthday',
   },
   {
@@ -36,14 +43,22 @@ const App = () => {
     name: 'password',
     type: 'password',
     placeholder: 'Password',
+    errorMessage: `Password should be 8-20 characters and should include at 
+    least 1 letter and 1 number and 1 special character`,
     label: 'Password',
+    // eslint-disable-next-line max-len
+    pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+    required: true,
   },
   {
     id: 5,
     name: 'confirmPassword',
     type: 'password',
     placeholder: 'Confirm Password',
+    errorMessage: 'Passwords don\'t match',
     label: 'Confirm Password',
+    pattern: values.password,
+    required: true,
   },
   ]
   const handleSubmit = (e) => {
@@ -54,10 +69,10 @@ const App = () => {
   }
   console.log(values)
 
-
   return (
     <div className='app'>
       <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
         {inputs.map((input) => (
           <FormInput
             key={input.id}
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,11 +1,24 @@
+import {useState} from 'react'
 import './formInput.css'
 
 const FormInput = (props) => {
-  const {label, onChange, ...otherProps} = props
+  const [focused, setFocused] = useState(false)
+  const {label, errorMessage, onChange, ...inputProps} = props
+  const handleBlur = (_) => {
+    setFocused(true)
+  }
   return (
     <div className='formInput'>
       <label>{label}</label>
-      <input {...otherProps} onChange={onChange}/>
+      <input {...inputProps}
+      onChange={onChange}
+      onBlur={handleBlur}
+      // due to confirm password field should validate when typing
+      onFocus={() =>
+        inputProps.name === 'confirmPassword' && setFocused(true)
+      }
+      focused={focused.toString()} />
+      <span>{errorMessage}</span>
     </div>
   )
 }
